de27d7fda9b0b4547f849cf59fe57525a21d2533
黄亚州 <1024105345@qq.com>
2017-05-31 09:30:28 +0800

    显示进度

--- BreakPointDownload/ViewController.m
+++ BreakPointDownload/ViewController.m
@@ -12,7 +12,7 @@
 
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UIProgressView *downloadProgressView;
 @end
 
 @implementation ViewController
@@ -25,13 +25,14 @@
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     BPDownloadContent *aContent = [[BPDownloadContent alloc] init];
-    aContent.urlString = @"http://static.meishubao.com/video/2016-05-18/mCah6DSkD5.mp4";//http://hb-video.oss-cn-beijing.aliyuncs.com/20/1/0/227/f6ed9ef063ba51ff9440959c70fce0e8.mp4
+    aContent.urlString = @"http://hb-video.oss-cn-beijing.aliyuncs.com/20/1/0/227/f6ed9ef063ba51ff9440959c70fce0e8.mp4";//
     aContent.filePath = @"download/video";
     aContent.fileName = @"mCah6DSkD5.mp4";
     aContent.extenInfo = @{@"11":@"11",@"22":@[@"aa",@"aa1"]};
     [[BPNetWorkService shared] downloadContent:aContent onProgress:^(int64_t completeBytes, int64_t totalBytes) {
         CGFloat progress = (double)completeBytes / (double)totalBytes;
         NSLog(@"进度：%.5f",progress);
+        self.downloadProgressView.progress = progress;
     } onComplete:^(BPDownloadContent* aContent, NSError *aError) {
         NSLog(@"%@",aContent.cacheFilePath);
         NSLog(@"%@",aError);
@@ -43,6 +44,13 @@
     // Dispose of any resources that can be recreated.
 }
 
-
+- (UIProgressView *)downloadProgressView {
+    if (nil == _downloadProgressView) {
+        _downloadProgressView = [[UIProgressView alloc] initWithFrame:CGRectMake(10, 100, 200, 30)];
+        
+        [self.view addSubview:_downloadProgressView];
+    }
+    return _downloadProgressView;
+}
 
 @end
